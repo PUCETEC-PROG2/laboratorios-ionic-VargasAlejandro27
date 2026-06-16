9fe526343638a5f6866a6b25cd352bea76d59dd6
VargasAlejandro27 <tu-email@gmail.com>
2026-06-16 14:52:21 -0500

    laboratorio 8

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonCard, IonCardContent, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
@@ -7,16 +6,29 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Perfil de usuario</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Perfil de usuario</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <div className="container">
+          <IonCard className="card">
+            <img
+              src="https://avatars.githubusercontent.com/u/191403759?v=4"
+              alt="Foto de perfil"
+            />
+            <IonCardTitle>Alejandro Vargas Saavedra</IonCardTitle>
+            <IonCardSubtitle>avvargas@puce.edu.ec</IonCardSubtitle>
+            <IonCardContent>
+              <p>Desarrollador de software con experiencia en el desarrollo de aplicaciones móviles y web.</p>
+              <p>Si deseas contactarme, puedes enviarme un correo electrónico a avvargas@puce.edu.ec</p>
+            </IonCardContent>
+          </IonCard>
+        </div>
       </IonContent>
     </IonPage>
   );
